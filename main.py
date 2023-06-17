c46a6f4681549a58ea8301b53269a5ea718befe6
Brian McDonald <brian.mcdonald@falmouth.ac.uk>
2023-06-17 23:02:58 +0100

    Added main file

---
+++ main.py
@@ -0,0 +1,9 @@
+#https://stackoverflow.com/questions/43146528/how-to-extract-all-the-emojis-from-text
+#https://towardsdatascience.com/a-beginners-guide-to-sentiment-analysis-in-python-95e354ea84f6
+
+def main():
+    print("Hello World")
+
+
+if __name__=="__main__":
+    main()
