0bf8666665d426f18459a98e5d764fb74c0eb110
Brian McDonald <brian.mcdonald@falmouth.ac.uk>
2023-06-21 17:00:38 +0100

    Main changed to run word cloud

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-#https://stackoverflow.com/questions/43146528/how-to-extract-all-the-emojis-from-text
-#https://towardsdatascience.com/a-beginners-guide-to-sentiment-analysis-in-python-95e354ea84f6
+
+
+
+from excel import read_files_from_directory,read_single_file
+from emoji import extract_emoji_data
+from word_cloud import create_most_used_word_cloud
+
+columns=["Group","Student name","Student email","Reviewed work of student name","Criterion","Kind","Review"]
+sheet_name='Reviews'
+directory='data'
 
 def main():
-    print("Hello World")
-
+    data_frame=read_files_from_directory(directory,sheet_name,columns)
+    #extract_emoji_data(data_frame,'Review',"Emoji")
+    create_most_used_word_cloud("most_used.png",data_frame)
+    #data_frame.to_excel("emoji_data.xlsx")
+    
 
 if __name__=="__main__":
     main()
